f1b4b524d6d5a514a00ba5b1a51fdaeb4f9b8886
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-02-06 00:27:53 -0600

    Added libraries for processes and coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.github.pgreze:kotlin-process:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 
 tasks.test {
